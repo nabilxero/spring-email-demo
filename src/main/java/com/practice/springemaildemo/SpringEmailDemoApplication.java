ea87f883f72ff63c63342efe744693217b52fadf
Nabil <nurahasnatnabil@gmail.com>
2021-02-14 15:04:49 +0600

    First commit

---
+++ src/main/java/com/practice/springemaildemo/SpringEmailDemoApplication.java
@@ -0,0 +1,13 @@
+package com.practice.springemaildemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmailDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmailDemoApplication.class, args);
+	}
+
+}
